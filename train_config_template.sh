da1e81f939a8ad06a1992a5c0f5f7c944eee54ba
Guillaume Boniface <boniface@MacBook-Pro.fritz.box>
2021-12-07 13:16:27 +0000

    Add a template for a bash utility helping populating the env variables required to run training on gcs

---
+++ train_config_template.sh
@@ -0,0 +1,11 @@
+# utility to populate env variables before running the train command line
+# replace the brackets by value then run the script to be ready to run the
+# training command
+now=$(date +"%Y%m%d_%H%M%S")
+export JOB_NAME="sr3_train_$now"
+export STAGING_BUCKET=[your staging bucket]
+export TFR_BUCKET=[bucket with tfds and train run data, including models]
+export JOB_DIR=gs://$TFR_BUCKET/train_output_$now
+export PACKAGE_PATH=./sr3
+export MODULE_NAME=sr3.trainer.task
+export REGION=[region to run]
