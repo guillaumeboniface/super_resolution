4b85081fb3b798a6bd2e0aae96b545e727c74437
Guillaume Boniface <boniface@MacBook-Pro.home>
2021-12-24 11:48:38 +0100

    Add functions for the Gaussian diffusion process based on the DDPM paper implementation

--- sr3/noise_utils.py
+++ sr3/noise_utils.py
@@ -54,8 +54,8 @@ def generate_noisy_image_batch(image: tf.Tensor, noise_param: tf.float32) -> Ite
     return tf.math.sqrt(noise_param) * image + tf.math.sqrt(1 - noise_param) * noise, noise
 
 def generate_noisy_image_minus_one_batch(
-    image: tf.Tensor,
-    noisy_img: tf.Tensor,
+    image_t0: tf.Tensor,
+    image_t: tf.Tensor,
     gamma: tf.float32,
     gamma_minus_one: tf.float32,
     alpha: tf.float32) -> tf.Tensor:
@@ -67,7 +67,28 @@ def generate_noisy_image_minus_one_batch(
     gamma = tf.reshape(gamma, [gamma.shape[0], 1, 1, 1])
     alpha = tf.reshape(alpha, [alpha.shape[0], 1, 1, 1])
     gamma_minus_one = tf.reshape(gamma_minus_one, [gamma_minus_one.shape[0], 1, 1, 1])
-    mu = tf.math.sqrt(gamma_minus_one) * (1 - alpha) / (1 - gamma) * image + \
-            tf.math.sqrt(alpha) * (1 - gamma_minus_one) / (1 - gamma) * noisy_img
-    sigma = tf.math.sqrt((1 - gamma_minus_one) * (1 - alpha) / (1 - gamma))
-    return mu + sigma * tf.random.normal(image.shape, stddev=1.)
+    mu = tf.math.sqrt(gamma_minus_one) * (1 - alpha) / (1 - gamma) * image_t0 + \
+            tf.math.sqrt(alpha) * (1 - gamma_minus_one) / (1 - gamma) * image_t
+    sigma = tf.math.log(tf.maximum((1 - gamma_minus_one) * (1 - alpha) / (1 - gamma), 1e20))
+    z = tf.random.normal(image_t0.shape, stddev=1.) if gamma_minus_one < 1. else tf.zeros(image_t0.shape)
+    return mu + tf.random.normal(image_t0.shape, stddev=1.) * tf.math.exp(0.5 * sigma)
+
+def generate_image_minus_one(
+    image_t: tf.Tensor,
+    predicted_noise: tf.Tensor,
+    gamma: tf.Tensor,
+    gamma_minus_one: tf.Tensor,
+    alpha: tf.Tensor) -> tf.Tensor:
+
+    img_start_guess = predict_start_img(image_t, predicted_noise, gamma)
+    img_start_guess = tf.clip_by_value(img_start_guess, -1., 1.)
+    image_t_minus_one = generate_noisy_image_minus_one_batch(img_start_guess, image_t, gamma, gamma_minus_one, alpha)
+
+    return image_t_minus_one
+
+def predict_start_img(
+    image_t: tf.Tensor,
+    predicted_noise: tf.Tensor,
+    gamma: tf.Tensor) -> tf.Tensor:
+
+    return (image_t - predicted_noise * tf.math.sqrt(1 - gamma)) / tf.math.sqrt(gamma)
